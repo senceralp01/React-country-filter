49d597d540b57eb9fd500175bf69cc1300df2779
sencer2022 <senceralpdogdu@gmail.com>
2022-11-12 20:27:10 +0300

    fixed

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Error = (props) => {
   return (
     <div className="d-flex justify-content-center mt-3">
-      <div className="alert alert-dark w-25 text-center fst-italic fw-bold text-secondary" role="alert">
+      <div className="alert alert-dark w-50 text-center fst-italic fw-bold text-secondary" role="alert">
         {props.info}
       </div>
     </div>
